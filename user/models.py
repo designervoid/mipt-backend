6c4d2113d2d569111eabbed31a5e1806cd12b38d
nikit34 <permikov134@mail.ru>
2020-02-29 15:54:02 +0300

    second

--- user/models.py
+++ user/models.py
@@ -4,20 +4,14 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class User(models.Model):
+    id = models.ForeignKey(on_delete=models.DO_NOTHING)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    room = models.IntegerField()
+    group = models.CharField(max_length=5)
+    passwd = models.CharField(max_length=20)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
+        return self.user_text
